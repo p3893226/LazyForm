82eb1368c40bedbb7e0c920b4ce9fac9004cad31
p3893226 <p3893226@gmail.com>
2022-01-30 14:48:26 +0800

    hw3完成

--- src/Form/hook/useInputs.js
+++ src/Form/hook/useInputs.js
@@ -9,6 +9,7 @@ const useInputs = (inputValue = "") => {
     phoneNumber: "",
     question: "",
     type: "",
+    other: "",
   };
   const blankData = {
     nickname: null,
@@ -24,16 +25,25 @@ const useInputs = (inputValue = "") => {
     let isBlankCopy = { ...isBlankData };
     e.preventDefault();
     Object.entries(data).forEach(([key, value]) => {
-      if (!value.trim()) {
-        isBlankCopy[key] = true;
-      } else {
+      if (!!value.trim() & (key !== "other")) {
         isBlankCopy[key] = false;
         counter += 1;
+      } else {
+        isBlankCopy[key] = true;
       }
     });
     setIsBlankData(isBlankCopy);
-    if (counter === 5) {
-      alert(`報名成功`);
+    if (counter >= 5) {
+      alert(`報名資料如下:
+      暱稱: ${data.nickname}
+      電子郵件: ${data.email}
+      手機號碼: ${data.phoneNumber}
+      如何知道活動: ${data.question}
+      報名類型: ${data.type}
+      其他:${data.other}
+
+      報名成功!!
+      `);
     } else {
       alert(`請確認資料`);
     }
